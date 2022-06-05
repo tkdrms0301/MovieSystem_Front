a5aee926523af837b6846807c33a380b53f4fc62
yujin <scab12341@gmail.com>
2022-06-05 23:31:29 +0900

    상근이 밥줘

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeLayout from './HomeLayout';
 import Movie from './components/Movie';
+import Home from './components/Home';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Router>
                 <HomeLayout>
                     <Routes>
+                        <Route exact path="/" element={<Home />} />
                         <Route exact path="/movies" element={<Movie />} />
                         <Route exact path="/theaters"></Route>
                     </Routes>
