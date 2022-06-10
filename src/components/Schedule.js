320a2597d118188046460addf2a0ec1eb3d24878
cjfals9626 <81482706+cjfals9626@users.noreply.github.com>
2022-06-11 06:54:13 +0900

    link

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,13 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Schedule({ movieSchedule }) {
     return (
         <li>
-            <a
-                href="/ticket/?MOVIE_CD=20029075&amp;MOVIE_CD_GROUP=20029075&amp;PLAY_YMD=20220605&amp;THEATER_CD=0056&amp;PLAY_START_TM=2000&amp;AREA_CD=13&amp;SCREEN_CD=003"
-                target="_top"
-            >
+            <Link to={'/ticketing'}>
                 <em>{movieSchedule?.time}</em>
                 <span class="txt-lightblue">98석</span>
-            </a>
+            </Link>
         </li>
     );
 }
